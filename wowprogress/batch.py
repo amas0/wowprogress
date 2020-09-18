ff7f78d6c2e119024bd30e4f94de0e60e26189e8
amas <andrew.mascioli1@gmail.com>
2020-09-18 11:11:31 -0400

    Add integration function with filtering capabilities

--- wowprogress/batch.py
+++ wowprogress/batch.py
@@ -1,7 +1,8 @@
 import json
 import gzip
+import itertools as it
 from dataclasses import dataclass
-from typing import List
+from typing import Callable, Generator, List, Optional
 
 import bs4
 import requests
@@ -51,23 +52,37 @@ def get_links(soup: bs4.BeautifulSoup) -> List[BatchRankingLink]:
     return [BatchRankingLink.from_url(url) for url in urls]
 
 
-def list_files() -> List[BatchRankingLink]:
-    res = requests.get(URL)
+def list_files(session: Optional[requests.Session] = None) -> List[BatchRankingLink]:
+    res = session.get(URL) if session else requests.get(URL)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, 'html.parser')
     links = get_links(soup)
     return links
 
 
-def download_export(link: str) -> dict:
-    res = requests.get(link)
+def download_export(link: str, session: Optional[requests.Session] = None) -> dict:
+    res = session.get(link) if session else requests.get(link)
     res.raise_for_status()
     unzipped = gzip.decompress(res.content)
     return json.loads(unzipped)
 
 
-def get_export_rankings(link: BatchRankingLink) -> List[BatchRanking]:
-    dl_rankings = download_export(link.export_url)
-    rankings = [BatchRanking(**rank, area=link.area, realm=link.realm, tier=link.tier)
-                for rank in dl_rankings]
-    return rankings
+def get_export_rankings_from_link(link: BatchRankingLink,
+                                  session: Optional[requests.Session] = None) -> Generator[BatchRanking, None, None]:
+    dl_rankings = download_export(link.export_url, session=session)
+    rankings = (BatchRanking(**rank, area=link.area, realm=link.realm, tier=link.tier)
+                for rank in dl_rankings)
+    yield from rankings
+
+
+def get_export_rankings(area: str = '', realm: str = '', tier: int = 0,
+                        filter_fn: Callable[[BatchRankingLink], bool] = None):
+    if filter_fn is None:
+        def filter_fn(link):
+            return (((area == '') or (link.area == area)) and
+                    ((realm == '') or (link.realm == realm)) and
+                    ((tier == 0) or (link.tier == tier)))
+    with requests.Session() as session:
+        to_download = [link for link in list_files(session) if filter_fn(link)]
+        rankings = (get_export_rankings_from_link(link, session) for link in to_download)
+        yield from it.chain(*rankings)
